50cdee6607627d60d25d55441b8e5f9f44d14941
Kamlesh <kamleshchandela72@gmail.com>
2026-02-11 00:00:24 +0530

    react notes added

--- prasantsirNotes/react1/src/components/addTodo.jsx
+++ prasantsirNotes/react1/src/components/addTodo.jsx
@@ -1,10 +1,46 @@
+import TodoItems from "./todoitems"
+
+function creat () {
+    return (
+        <div className="data1">
+          <p className='data11'>
+            {todoName}
+          </p>
+          <p className='data11'>
+            {todoData}
+          </p>
+          <button className="red-btn">DELETE</button>
+        </div>
+    )
+}
+
+
 function AddTodo(){
     return (
+        <>
         <div className="data1">
           <input type="text" name="" className='input' placeholder='Enter To Do...' id="" />
           <input type="date" name="" className='date' id="" />
-          <button className="green-btn">ADD</button>
+          <button onClick={creat} className="green-btn">ADD</button>
         </div>
+
+        <TodoItems />
+
+            {function one (params) {
+                <div className="data1">
+                <p className='data11'>
+                    {todoName}
+                </p>
+                <p className='data11'>
+                    {todoData}
+                </p>
+                <button className="red-btn">DELETE</button>
+                </div>
+            }}
+
+
+
+        </>
     )
 }
 
